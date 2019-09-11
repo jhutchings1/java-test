2334ce73287f480c98c09e9aa12d5cd70f590629
sjvs <github@s.traiectum.net>
2019-09-11 19:38:32 +0100

    further work

--- src/Octocat.java
+++ src/Octocat.java
@@ -1,4 +1,10 @@
-public class Octocat {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+public class Octocat implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String name;
 
     public Octocat(String name) {
@@ -7,5 +13,11 @@ public class Octocat {
 
     public void dance() {
         System.out.println("My name is " + this.name + " and I'm doing a dance!");
+    }
+    
+    public static Octocat deserializeOctocat(InputStream stream) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream in = new ObjectInputStream(stream)) {
+            return (Octocat) in.readObject();
+        }
     }
 }
--- src/OctocatManager.java
+++ src/OctocatManager.java
@@ -1,7 +1,14 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class OctocatManager {
 
-    public static void main (String[] args) {
-        Octocat mona = new Octocat("Mona");
-        mona.dance();
+    public static void main (String[] args) throws Exception {
+        try (ServerSocket serverSocket = new ServerSocket(1234)){
+            Socket socket = serverSocket.accept();
+            Octocat mona = Octocat.deserializeOctocat(socket.getInputStream());
+            
+            mona.dance();
+        }
     }
 }
