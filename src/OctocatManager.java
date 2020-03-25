2ccc94f1d55273f5c073f957052d2e98ab5140ab
Justin Hutchings <jhutchings1@users.noreply.github.com>
2020-03-25 09:57:30 -0700

    Add comment in Chinese

--- src/OctocatManager.java
+++ src/OctocatManager.java
@@ -6,6 +6,7 @@ public class OctocatManager {
     public static void main (String[] args) throws Exception {
         try (ServerSocket serverSocket = new ServerSocket(1234)){
             Socket socket = serverSocket.accept();
+            // ①Ⅻㄨㄩ 啊阿鼾齄丂丄狚狛狜狝﨨﨩ˊˋ˙–⿻〇㐀㐁㐃㐄䶴䶵U1[]U2[]U3[]
             Octocat mona = Octocat.deserializeOctocat(socket.getInputStream());
             
             mona.dance();
